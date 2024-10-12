d5ca2d148059b2fba8866e8879abcd8226f0c574
Ali Asadpoor <aliasadpoor2.1@gmail.com>
2024-10-11 22:49:02 -0400

    Changed layout dimensions

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -22,16 +22,16 @@ export function ProjectsPosts() {
             href={`/projects/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-blue-700 dark:text-blue-400 underline tracking-tight cursor-pointer transition-transform duration-200 ease-out hover:scale-105 hover:text-blue-900 dark:hover:text-blue-300">
+              <p className="text-lg text-blue-700 dark:text-blue-400 underline tracking-tight cursor-pointer transition-transform duration-200 ease-out hover:scale-102 hover:text-blue-900 dark:hover:text-blue-300">
                 {post.metadata.title}
+              </p>
+              <p className="text-sm text-neutral-600 dark:text-neutral-400 w-[300px] tabular-nums">
+                {formatDate(post.metadata.publishedAt, false)}
               </p>
             </div>
 
             {/* Display key technologies on the main projects page */}
-            <p className="text-sm text-blue-300 dark:text-blue-200">
+            <p className="text-sm text-white-300 dark:text-white-200">
               Technologies: {post.metadata.techs}
             </p>
           </Link>
